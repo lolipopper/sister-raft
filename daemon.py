db54d35d7311c1895be8351fa97bd39a4c081a0c
Azudeus <blitzthunder68@yahoo.com>
2017-04-28 05:36:49 +0700

    Better daemon structure

--- daemon.py
+++ daemon.py
@@ -8,18 +8,26 @@ PORT = 12221;
 class DaemonHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         try:
-            r = requests.get("http://localhost:13337/2")
+            args = self.path.split('/')
+
             self.send_response(200)
             self.end_headers()
-            self.wfile.write((str(r.text)+ "&endl").encode('utf-8'))
-            self.wfile.write(str(self.getCpuLoad()).encode('utf-8'))
+
+            if len(args) != 2:
+                raise Exception()
+
+            if args[1].isdigit():
+                n = int(args[1])
+                r = requests.get("http://localhost:13337/"+str(n))
+                self.wfile.write((str(r.text)).encode('utf-8'))
+            else:
+                self.wfile.write(str(self.getCpuLoad()).encode('utf-8'))
         except Exception as ex:
             self.send_response(500)
             self.end_headers()
             print(ex)
     def getCpuLoad(self):
         return psutil.cpu_percent(interval=1)
-
 
 server = HTTPServer(("", PORT), DaemonHandler)
 server.serve_forever()
