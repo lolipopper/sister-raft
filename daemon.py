b520f42033a65e0040fad62b1c37f48c9890ceb4
Azudeus <blitzthunder68@yahoo.com>
2017-04-28 07:27:47 +0700

    Added multithreading and sending to balancer nodes

--- daemon.py
+++ daemon.py
@@ -1,9 +1,24 @@
+from socketserver import ThreadingMixIn
 from http.server import HTTPServer
 from http.server import BaseHTTPRequestHandler
 import psutil
 import requests
+import threading
+import json
+import uuid
 
 PORT = 12221;
+URL = "";
+ID = -999;
+
+def sendStatus():
+    load = getCpuLoad();
+    headers = {'content-type' : 'application/json'}
+    data = {"load":load,"id":ID}
+    requests.post(URL,data=data)
+
+def getCpuLoad():
+    return psutil.cpu_percent(interval=1)
 
 class DaemonHandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -21,15 +36,27 @@ class DaemonHandler(BaseHTTPRequestHandler):
                 r = requests.get("http://localhost:13337/"+str(n))
                 self.wfile.write((str(r.text)).encode('utf-8'))
             else:
-                self.wfile.write(str(self.getCpuLoad()).encode('utf-8'))
+                self.wfile.write(str(getCpuLoad()).encode('utf-8'))
         except Exception as ex:
             self.send_response(500)
             self.end_headers()
             print(ex)
-    def getCpuLoad(self):
-        return psutil.cpu_percent(interval=1)
 
-server = HTTPServer(("", PORT), DaemonHandler)
-server.serve_forever()
+class ThreadingServer(ThreadingMixIn,HTTPServer):
+    pass
+
+if(ID==-999):
+    ID = uuid.uuid4()
+
+# start the server in a background thread
+server = ThreadingServer(('localhost',PORT),DaemonHandler)
+server_thread = threading.Thread(target=server.serve_forever)
+server_thread.daemon = True
+server_thread.start()
+
+while True:
+    print (ID)
+    sendStatus()
+    print(str(getCpuLoad()))
 
 
